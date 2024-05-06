789f938c724deec198b413b9c16fb5047e73e53f
Isamar Banos <Isamargonzalez90@gmail.com>
2024-05-06 14:00:58 -0400

    first draft

--- natureslovetouch/src/Components/Navbar/Navbar.jsx
+++ natureslovetouch/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 
 function Navbar (){
-
+    const [menu, setMenu] = useState("shop");
+    
     return(
         <div className = 'navbar'>
             <div className='nav-logo'>
@@ -12,11 +13,11 @@ function Navbar (){
                 <p>SHOPPER</p>
             </div>
         <ul className='nav-menu'>
-            <li>Shop<hr/></li>
-            <li>Cleansing & Smudging</li>
-            <li>Essential Oil</li>
-            <li>Crystal Jewelry</li>
-            <li>Crystal | Gemstone</li>
+            <li onClick={() => setMenu("shop")}>Shop{menu === "shop" ?<hr/>:<></>}</li>
+            <li onClick={() => setMenu("cleansing")}>Cleansing & Smudging{menu === "cleansing" ?<hr/>:<></>}</li>
+            <li onClick={() => setMenu("essential oil")}>Essential Oil{menu === "essential oil" ?<hr/>:<></>}</li>
+            <li onClick={() => setMenu("crystal jewelry")}>Crystal Jewelry{menu === "crystal jewelry" ?<hr/>:<></>}</li>
+            <li onClick={() => setMenu("crystals")}>Crystal | Gemstone{menu === "crystals" ?<hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
         <button>Login</button>
