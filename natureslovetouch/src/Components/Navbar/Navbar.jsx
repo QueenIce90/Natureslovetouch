12c28fcff157b67f9135f7287c6841b626c4b319
Isamar Banos <Isamargonzalez90@gmail.com>
2024-05-05 23:46:49 -0400

    first draft

--- natureslovetouch/src/Components/Navbar/Navbar.jsx
+++ natureslovetouch/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 
 function Navbar (){
 
     return(
         <div className = 'navbar'>
-
+            <div className='nav-logo'>
+                <img src={logo} alt=''/>
+                <p>SHOPPER</p>
+            </div>
+        <ul className='nav-menu'>
+            <li>Shop</li>
+            <li>Cleansing & Smudging</li>
+            <li>Essential Oil</li>
+            <li>Crystal Jewelry</li>
+            <li>Crystal / Gemstone</li>
+        </ul>
+        <div className="nav-login-cart">
+        <button>Login</button>
+        <img src= {cart_icon} alt=''/>
+        </div>
         </div>
     )
 }
